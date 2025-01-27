ec64ef3e539ae84ebad50a1e916df15d62b21226
Isabela Nunes Zeferino <144686519+IsabelaNz@users.noreply.github.com>
2025-01-27 20:27:36 -0300

    exercício 3

--- src/EX_3.java
+++ src/EX_3.java
@@ -1,2 +1,15 @@
+import java.util.Scanner;
+
 public class EX_3 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite seu nome: ");
+        String nome = scanner.nextLine();
+
+        System.out.println("Qual é o seu salário: ");
+        double salario = scanner.nextDouble();
+
+        System.out.println("O funcionário " + nome + " tem um salário de " + salario + " em junho.");
+    }
 }
