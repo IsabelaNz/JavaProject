a4f56d228585cca73f4419ec9f3046ca15140729
Isabela Nunes Zeferino <144686519+IsabelaNz@users.noreply.github.com>
2025-01-28 20:26:24 -0300

    exercício 9

--- src/EX_9.java
+++ src/EX_9.java
@@ -4,7 +4,7 @@ public class EX_9 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Quanto de dinheiro você tem na carteira ?");
+        System.out.println("Quanto dinheiro você tem na carteira ?");
         double real = scanner.nextDouble();
 
         double cotacao = 3.45;
