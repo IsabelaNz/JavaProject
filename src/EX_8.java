6bd96dd8917d8dc23da8f8b9a8184b8d5da44849
Isabela Nunes Zeferino <144686519+IsabelaNz@users.noreply.github.com>
2025-01-28 20:10:46 -0300

    exercício 8

---
+++ src/EX_8.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class EX_8 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite uma distância em metros: ");
+        Double metros = scanner.nextDouble();
+
+        Double centimetros = metros * 100;
+        System.out.println("A distância de " + metros + " metros corresponde a " + centimetros + " centímetros.");
+
+        double quilometros = metros / 1000;
+        System.out.println("A distância de " + metros + " metros corresponde a " + quilometros + " quilometros.");
+
+        double hectometro = metros / 100;
+        System.out.println("A distância de " + metros + " metros corresponde a " + hectometro + " hectometros.");
+
+        double decametro = metros / 10;
+        System.out.println("A distância de " + metros + " metros corresponde a " + decametro + " decametros.");
+
+        double decimetro = metros * 10;
+        System.out.println("A distância de " + metros + " metros corresponde a " + decimetro + " decimetros.");
+
+        double milimetros = metros * 1000;
+        System.out.println("A distância de " + metros + " metros corresponde a " + milimetros + " milimetros.");
+
+
+        scanner.close();
+    }
+}
