5df115100c56b9dd9f4e30b02619398165b2918a
Isabela Nunes Zeferino <144686519+IsabelaNz@users.noreply.github.com>
2025-01-28 19:43:00 -0300

    exercício 6

---
+++ src/EX_6.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class EX_6 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite um número inteiro: ");
+        int numero = scanner.nextInt();
+
+        int antecessor = numero - 1;
+        int sucessor = numero + 1;
+
+        System.out.println("O antecessor de " + numero + " é " + antecessor);
+        System.out.println("O sucessor de " + numero + " é " + sucessor);
+
+    }
+}
