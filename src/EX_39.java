4b69369c408f633007f5ba8655dde59c0e1d7b7b
Isabela Nunes Zeferino <144686519+IsabelaNz@users.noreply.github.com>
2025-02-27 18:32:28 -0300

    exercício 39

---
+++ src/EX_39.java
@@ -0,0 +1,8 @@
+public class EX_39 {
+    public static void main(String[] args) {
+        for(int i = 10; i >= 3; i --) {
+            System.out.print(i + " ");
+        }
+        System.out.println("Acabou!");
+    }
+}
