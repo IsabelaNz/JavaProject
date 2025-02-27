90873108426da8429ec7360aaf9a67cd05bdb2d7
Isabela Nunes Zeferino <144686519+IsabelaNz@users.noreply.github.com>
2025-02-27 20:39:49 -0300

    exercício 40

---
+++ src/EX_40.java
@@ -0,0 +1,8 @@
+public class EX_40 {
+    public static void main(String[] args) {
+        for (int i = 0; i <= 18; i += 3) { // incrementa 3
+            System.out.print(i + " ");
+        }
+        System.out.println("Acabou!");
+    }
+}
