9780abe3654bf5a4b81217f9c76d27dd8d507f6b
Isabela Nunes Zeferino <144686519+IsabelaNz@users.noreply.github.com>
2025-03-07 11:49:04 -0300

    exercício 43

---
+++ src/EX_43.java
@@ -0,0 +1,12 @@
+public class EX_43 {
+    public static void main(String[] args) {
+
+        for (int i = 0; i <= 30; i++) {
+            if (i % 4 == 0) {
+                System.out.print("[" + i + "] ");
+            } else {
+                System.out.print(i + " ");
+            }
+        }
+    }
+}
