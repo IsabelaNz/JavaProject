9c010bc24dc172b3682caa2b1997532f4769b6fc
Isabela Nunes Zeferino <144686519+IsabelaNz@users.noreply.github.com>
2025-05-16 09:15:58 -0300

    exercício 61

---
+++ src/EX_61.java
@@ -0,0 +1,14 @@
+
+public class EX_61 {
+    public static void main(String[] args) {
+
+        int i = 0;
+
+        do {
+            System.out.println("i = " + i);
+            i += 3;
+
+        } while (i <= 30);
+        System.out.println("Acabou!");
+    }
+}
