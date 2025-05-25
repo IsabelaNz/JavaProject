fcafea4186324863f728cba093514c11b9c1fc64
Isabela Nunes Zeferino <144686519+IsabelaNz@users.noreply.github.com>
2025-05-24 22:01:49 -0300

    Create Ex_64.java

---
+++ src/Ex_64.java
@@ -0,0 +1,8 @@
+public class EX_64 {
+    public static void main(String[] args) {
+        for(int i = 0; i <= 40; i += 5){
+            System.out.println(i);
+        }
+        System.out.println("Acabou!");
+    }
+}
