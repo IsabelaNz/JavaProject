2d00c8768f0617a4ccc537a09eece3cc9c9317aa
Isabela Nunes Zeferino <144686519+IsabelaNz@users.noreply.github.com>
2025-05-29 19:12:19 -0300

    Delete src/Ex_64.java

--- src/Ex_64.java
+++
@@ -1,8 +0,0 @@
-public class EX_64 {
-    public static void main(String[] args) {
-        for(int i = 0; i <= 40; i += 5){
-            System.out.println(i);
-        }
-        System.out.println("Acabou!");
-    }
-}
